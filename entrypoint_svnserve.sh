7e87a9b8f5be632d81bb74fa3bb39c7cb8863821
devel.msuzuhara <devel.msuzuhara@gmail.com>
2023-02-06 23:45:31 +0900

    update repos setting

--- entrypoint_svnserve.sh
+++ entrypoint_svnserve.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+if [[ -z "${SUBVERSION_REPOS}" ]]; then
+    SUBVERSION_REPOS=/var/svn/repos
+fi
+
 /usr/local/subversion/bin/svnserve --daemon --foreground --root=${SUBVERSION_REPOS}
